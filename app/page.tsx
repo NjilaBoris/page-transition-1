d5f4214a57e0f9eb8f10a0773ad5b7cb72a2ef49
NjilaBoris <njilaboris2@gmail.com>
2025-10-28 00:01:23 +0100

    Final

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,38 @@
 "use client";
 import { useRef } from "react";
-import Copy from "@/app/components/Copy";
+
+
+import {SplitText} from "gsap/SplitText";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import ReactLenis from "lenis/react";
+
+
+gsap.registerPlugin(useGSAP);
 
 export default function Home() {
     const container = useRef(null);
 
+    useGSAP(
+        () => {
+            const heroText = new SplitText(".home h1", { types: "chars" });
+            gsap.set(heroText.chars, { y: 400 });
+
+            gsap.to(heroText.chars, {
+                y: 0,
+                duration: 1,
+                stagger: 0.075,
+                ease: "power4.out",
+                delay: 1,
+            });
+        },
+        { scope: container }
+    );
+
     return (
         <ReactLenis root>
             <div className="home" ref={container}>
-                <Copy>
                 <h1>Kaelon</h1>
-                </Copy>
             </div>
         </ReactLenis>
     );
